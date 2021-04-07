90581c840f67970660f9b5f04600ce2209c9a161
Kabir Singh Shekhawat <kabirsinghshekhawat1789@gmail.com>
2021-04-07 22:27:25 +0530

    Add MongoDB connection

---
+++ db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+exports.connectDatabase = () => {
+    mongoose.connect('mongodb://localhost/nlpDemo', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: true
+    })
+        .then(() => {
+            console.log('Connected To Database')
+        })
+        .catch(err => {
+            throw new Error('Error connecting:\n')
+        })
+}
