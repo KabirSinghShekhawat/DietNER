fe9edf5f15e3ff203e570a873d7fcc12a07b96a4
Kabir Singh Shekhawat <kabirsinghshekhawat1789@gmail.com>
2021-04-07 21:15:07 +0530

    Fix Submit Button Size

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const express = require('express')
 const app = express()
+
 const path = require('path')
 
 // config
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+
 // middleware
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ limit: '10kb', extended: true }))
 app.use(express.json({ limit: '10kb'}))
 
 
 app.get('/', (req, res) => {
-    res.status(200).send('Success')
+    res.status(200).render('index')
 })
 
 module.exports = app
