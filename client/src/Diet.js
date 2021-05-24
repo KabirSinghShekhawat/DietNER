abae84fd2313d1df7e1b07f15d0c8e420d408d4e
Kabir Singh Shekhawat <kabirsinghshekhawat1789@gmail.com>
2021-05-24 14:16:11 +0530

    Add Diet Plan Render with basic styling

--- client/src/Diet.js
+++ client/src/Diet.js
@@ -1,4 +1,4 @@
-import {Component} from "react";
+import React, {Component} from "react";
 import Axios from "axios";
 
 class Diet extends Component {
@@ -18,16 +18,25 @@ class Diet extends Component {
 
     displayDiet() {
         const diet = this.state.diet.diet
-        if (typeof diet == 'undefined' || diet.length === 0) return ''
+        if (typeof diet == 'undefined' || diet.length === 0) return 'loading...'
         const dietList = diet.map((food) => {
-            return <li key={food._id}>{food.foodName}</li>
+            return <li className="list-group-item" key={food._id}>{food.foodName}</li>
         })
-        return <ul> {dietList} </ul>
+        return <ul className="list-group"> {dietList} </ul>
     }
 
     render() {
+        const diet = this.state.diet
+        const name = (typeof diet.name == 'undefined' || diet.name.length === 0)
+                    ? '' :
+                    <h3 className="text-lg-center">{this.state.diet.name}'s Diet Plan</h3>
         return (
-            <div>{this.displayDiet()}</div>
+            <React.Fragment>
+                <div className="container-fluid mt-3">
+                    {name}
+                </div>
+                <div className="container my-5">{this.displayDiet()}</div>
+            </React.Fragment>
         )
     }
 }
