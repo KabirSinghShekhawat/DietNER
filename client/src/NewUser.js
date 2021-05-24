8be0bba2a4f4a24996f69c1620630ee5e2292ea9
Kabir Singh Shekhawat <kabirsinghshekhawat1789@gmail.com>
2021-05-24 22:41:19 +0530

    Add New User

--- client/src/NewUser.js
+++ client/src/NewUser.js
@@ -1,5 +1,54 @@
-function NewUser() {
-    return <h1>New User Page</h1>
+import React, {Component} from "react";
+import Axios from "axios";
+
+class NewUser extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: ''
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    async handleSubmit(evt) {
+        evt.preventDefault()
+        await Axios.post(`http://localhost:3000/user`, {
+            name: evt.target.value
+        })
+            .then(function (response) {
+                //handle success
+                console.log(response.data)
+
+            })
+            .catch(function (error) {
+                //handle error
+                console.log(error.response);
+            });
+    }
+
+    handleChange(evt) {
+        this.setState({[evt.target.name]: evt.target.value})
+    }
+
+    render() {
+        return (
+            <div className="container my-5">
+                <form action="/user" method="POST">
+                    <div className="mb-3">
+                        <label htmlFor="name" className="form-label">Name: </label>
+                        <input type="text"
+                               className="form-control"
+                               id="name" name="name"
+                               aria-describedby="name"
+                               onClick={this.handleChange}
+                        />
+                        <div id="nameHelp" className="form-text">Please enter your name to register.</div>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default NewUser;
